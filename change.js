95030d9f3a4e3ba9ce78fac426a69012e52ae24b
Sathwikcm1 <sathwikcm@gmail.com>
2024-03-01 13:35:42 +0530

    Version 11

--- change.js
+++ change.js
@@ -1,2 +1,3 @@
 console.log("change")
-console.log("just added that shit into my actual github thingy..")
+console.log("just added that shit into my actual github thingy..");
+console.log('Versino 11 momma');
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 console.log('config.js');
 console.log('This not updated yet');
 prompt('So i am giving you a chance to edit this program now you can do whatever you want to do hehe.')
+console('Version 11')
